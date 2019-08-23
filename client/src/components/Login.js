f4b5f3ea2c1dec986abf815f37cef083edc325cd
Virginia Thayer <virginia@stripe.com>
2019-08-23 08:54:01 -0700

    BubblePage successfully fetches colors on mount and sets them to state.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,72 +2,72 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Login = (props) => {
-  const [creds, setCreds] = useState({
-    username: '',
-    password: ''
-});
+    const [creds, setCreds] = useState({
+        username: '',
+        password: ''
+    });
 
-function handleChange(e){
-    setCreds({...creds, [e.target.name]: e.target.value });
-}
+    function handleChange(e) {
+        setCreds({ ...creds, [e.target.name]: e.target.value });
+    }
 
-function login(e){
-    e.preventDefault();
-    axios
-        .post(' http://localhost:5000/api/login', creds)
-        .then(res => {
-            console.log(res);
-            localStorage.setItem('token', res.data.payload);
-            setCreds({
-                username: '',
-                password: ''
+    function login(e) {
+        e.preventDefault();
+        axios
+            .post(' http://localhost:5000/api/login', creds)
+            .then(res => {
+                console.log(res);
+                localStorage.setItem('token', res.data.payload);
+                setCreds({
+                    username: '',
+                    password: ''
+                });
+                props.history.push("/protected");
+            })
+            .catch(err => {
+                console.log(err.response.data.error);
+                // setLoginStatus(err.response.data.error);
+                // setCreds({
+                //     username: '',
+                //     password: ''
+                // })
             });
-            props.history.push("/protected");
-        })
-        .catch(err => {
-            console.log(err.response.data.error);
-            // setLoginStatus(err.response.data.error);
-            // setCreds({
-            //     username: '',
-            //     password: ''
-            // })
-        });
-}
+    }
 
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <div>
+    // make a post request to retrieve a token from the api
+    // when you have handled the token, navigate to the BubblePage route
+    return (
+        <div>
             <h1>We've got some amazing bubbles in here</h1>
-            <h3>But... they're private.</h3> 
-            <br/>
+            <h3>But... they're private.</h3>
+            <br />
             <h4>so LOG IN!</h4>
-            <br/>
+            <br />
             <form onSubmit={login}>
-                <label htmlFor="username">Username: 
-                <br/>
-                <input
-                    type="text"
-                    name="username"
-                    value={creds.username}
-                    onChange={handleChange}
-                />
+                <label htmlFor="username">Username:
+                <br />
+                    <input
+                        type="text"
+                        name="username"
+                        value={creds.username}
+                        onChange={handleChange}
+                    />
                 </label>
-                <br/>
-                <label htmlFor="password">Password: 
-                <br/>
-                <input 
-                    type="password"
-                    name="password"
-                    value={creds.password}
-                    onChange={handleChange}
-                />
+                <br />
+                <label htmlFor="password">Password:
+                <br />
+                    <input
+                        type="password"
+                        name="password"
+                        value={creds.password}
+                        onChange={handleChange}
+                    />
                 </label>
-                <br/>
+                <br />
                 <button>Now show me the bubbles!!!</button>
             </form>
         </div>
-  );
+    );
 };
 
 export default Login;
