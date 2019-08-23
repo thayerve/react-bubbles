ea104f62200c21d2d01725e0ed9f2e0cfc363cc6
Virginia Thayer <virginia@stripe.com>
2019-08-23 08:40:43 -0700

    Stage 1: Authentication complete. Login component successfully posts login, gets token, pushes to private route.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './utils/PrivateRoute'
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -9,11 +11,10 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Link to="/login"/>
+        <Route exact path="/" render={(props)=><Login {...props}/>} />
+        <PrivateRoute exact path="/protected" component={BubblePage} />
+        
       </div>
     </Router>
   );
